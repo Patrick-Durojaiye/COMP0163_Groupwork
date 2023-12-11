a6e80a44cd87e214b76162515f35184e6825949c
Patrick-Durojaiye <pt.durojaiye@gmail.com>
2023-12-11 13:36:13 +0000

    Hardhat config update

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  defaultNetwork: "Sepolia",
+  networks:{
+    Sepolia : {
+      url: process.env.RPC,
+      accounts: [process.env.PRIVATE_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
 };
